8269c6af810192564bc93c136ba7917915bcf5b9
Brynn Harris-Shanks <brynn.hs@dal.ca>
2025-03-18 14:55:53 +0100

    more code.

--- code/pages/average.py
+++ code/pages/average.py
@@ -10,9 +10,9 @@ from code.dataset import PhotometryDataset, BehaviorDataset, MergeDatasets
 from dash_local_react_components import load_react_component
 
 # Import visualization functions
-from visualize import generate_average_plot, generate_plots
+from code.visualize import generate_average_plot, generate_plots
 # Import utility for condition assignments mapping (e.g., {'mouse1': 1, 'mouse2': 3, ...})
-from utils import load_assignments
+from code.utils import load_assignments
 
 dash.register_page(__name__, path='/average')
 app = dash.get_app()
